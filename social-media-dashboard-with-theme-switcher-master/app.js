8d06f09c49d34c564837d90809006ffcbd03b7eb
Melina <melinacarrion1@hotmail.com>
2021-09-29 16:42:08 -0500

    Project 24 localstorage added

--- social-media-dashboard-with-theme-switcher-master/app.js
+++ social-media-dashboard-with-theme-switcher-master/app.js
@@ -1,6 +1,25 @@
-const  button=document.querySelector(".slide");
+const button = document.querySelector('.slide');
 
-button.addEventListener("click",()=>{
-    document.body.classList.toggle("light");
-    button.classList.toggle("active");
-})
+button.addEventListener("click", () => {
+    document.body.classList.toggle('light');
+    button.classList.toggle('active');
+
+    //LocalStorage for keeping the light mode and dark mode
+    if (document.body.classList.contains('light')) {
+        localStorage.setItem('light-mode', 'true');
+    }
+    else {
+        localStorage.setItem('light-mode', 'false');
+    }
+
+});
+
+//getting the current mode
+if (localStorage.getItem('light-mode') === 'true') {
+    document.body.classList.add('light');
+    button.classList.add('active');
+}
+else {
+    document.body.classList.remove('light');
+    button.classList.remove('active');
+}
